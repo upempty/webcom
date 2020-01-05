6300b2719f09b06cef944f740e943287ecf984aa
Fei Cheng <feicheng@FeideMacBook-Pro.local>
2020-01-05 16:28:09 +0800

    chain the logic

--- src/main.py
+++ src/main.py
@@ -5,57 +5,99 @@ import time
 import sys
 
 class WebSocketServer:
-    def __init__(self):
-        print ('server started')
+    def __init__(self, on_open=None, on_msg=None):
+        print ('WS server started')
+        self.ws = None
+        self.on_open = on_open
+        self.on_msg = on_msg
 
     def run_forever(self):
-        #createServer
-        #accept
-        #handshake_response
-        #callback(self, cb, *args) #use for on_xx(msg, ping)
+        Sock.create_server(('127.0.0.1', 9001))
+        conn = Sock.server_accept()
+        self.ws = WebSocketChannel(conn)
+        self.ws.response_handshake()
+        self._callback(self.on_open) #use for on_xx(msg, ping)
         while True:
             time.sleep(2)
-            #self.read()
-            ##self._callback(self.on_msg, *args))
+            self.read()
             print('server callback handling')
 
+    def read(self):
+        #1
+        print ('read on server')
+        msg = self.ws.read()
+        self._callback(self.on_msg, msg)
+        #2 ping or pong
+
+    def _callback(self, callback, *args):
+        print ('callback defination on server', args)
+        if callback:
+            callback(self, *args)
+
 class WebSocketClient:
-    def __init__(self):
-        print ('client started')
+    def __init__(self, on_open=None, on_msg=None):
+        print ('WS client started')
+        self.on_open = on_open
+        self.on_msg = on_msg
+
 
     def run_forever(self):
-        #connect()
-        #handshake_req()
+        addr = ('127.0.0.1', 9001)
+        conn = Sock.create_connect(addr)
+        self.ws = WebSocketChannel(conn)
+        self.ws.request_handshake()
+        self._callback(self.on_open) #use for on_xx(msg, ping)
         while True:
             time.sleep(2)
-            #self.read()
-            ##self._callback(self.on_msg, *args))
+            self.read()
             print('client callback handling')
+
+    def read(self):
+        #1
+        print ('read on client')
+        msg = self.ws.read()
+        self._callback(self.on_msg, msg)
+        #2 ping or pong
+
+    def _callback(self, callback, *args):
+        print ('callback defination on client', args)
+        if callback:
+            callback(self, *args)
+
+
        
 class Sock:
+    @classmethod
     def create_server(self, addr):
-        pass
+        print ('create server:{})'.format(addr))
 
-    def server_accept(self, server):
-        pass
+    @classmethod
+    def server_accept(self):
+        print ('server accept')
+        return 0
 
+    @classmethod
     def create_connect(self, addr):
-        pass
+        print ('create connect to {}'.format(addr))
+        return 0
 
 class WebSocketChannel:
     def __init__(self, sock):
         self.sock = sock    
+        print ('WebSocketChannel init')
     
     def write(self, data):
         pass
 
     def read(self):
-        pass
+        print ('WebSocketChannel read')
+        return 0
 
     def request_handshake(self):
-        pass
+        print ('request handshake')
+
     def response_handshake(self):
-        pass 
+        print ('reponse handshake')
     
     def request_ping(self):
         pass
@@ -92,6 +134,13 @@ class Pingpong:
     def encode_pong(self):
         pass
 
+def on_open(ws):
+    print ('on open init message')
+
+def on_msg(ws, *args):
+    print ('on message!!!!')
+
+
 if __name__=='__main__':
 
     print (sys.argv)
@@ -108,5 +157,7 @@ if __name__=='__main__':
     print('main entry:主入口')
     #wserver = WebSocketClient()
     ws = daemon_role.get(id, WebSocketClient)()
+    ws.on_open = on_open
+    ws.on_msg = on_msg
     ws.run_forever()
 
